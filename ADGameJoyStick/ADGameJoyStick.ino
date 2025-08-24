bd1df813efae5b116fb3eb9d3c9c8535a31aba4c
JkhTV <candzzang5283@gmail.com>
2025-08-24 15:52:35 +0900

    Add ADGameJoyStick

---
+++ ADGameJoyStick/ADGameJoyStick.ino
@@ -0,0 +1,21 @@
+void setup()
+{
+    Serial.begin(9600);
+
+    pinMode(8, INPUT_PULLUP);
+}
+
+void loop()
+{
+    Serial.print(analogRead(A1));
+    Serial.print(" ");
+    Serial.print(analogRead(A0));
+    Serial.print(" ");
+    Serial.println(digitalRead(8));
+
+    Serial.write(A1);
+    Serial.write(A0);
+    Serial.flush();
+    
+    delay(100);
+}
